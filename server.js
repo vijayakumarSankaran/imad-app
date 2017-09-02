e4b447bebfd181b1f6e5fb1871701b5e8a6dd20a
vijayakumarSankaran <30766520+vijayakumarSankaran@users.noreply.github.com>
2017-09-02 13:39:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function (req, res) {
+  res.send('This is article one ready to serve');
+});
+
+app.get('/article-twp', function (req, res) {
+  res.send('This is article twp ready to serve');
+});
+
+app.get('/article-three', function (req, res) {
+  res.send('This is article three ready to serve');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
