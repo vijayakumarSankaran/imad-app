6f223585c7704d74eb7640385c7ed2a81f332dd7
vijayakumarSankaran <30766520+vijayakumarSankaran@users.noreply.github.com>
2017-09-02 13:42:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/article-one', function (req, res) {
   res.send('This is article one ready to serve');
 });
 
-app.get('/article-twp', function (req, res) {
+app.get('/article-two', function (req, res) {
   res.send('This is article twp ready to serve');
 });
 
