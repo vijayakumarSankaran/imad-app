6231aacec2eb28d9e30fea9107c10f979d5f9ee7
vijayakumarSankaran <30766520+vijayakumarSankaran@users.noreply.github.com>
2017-09-03 20:22:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,11 @@ console.log('Loaded!');
 //Change the text of main-text div
 var element=document.getElementById('main-text');
 element.innerHTML= "New Code";
+
+//Move the image
+
+var moveImg= document.getElementById('image');
+moveImg.onclick= function()
+{
+    moveImg.style.marginLeft= '100px';
+};
