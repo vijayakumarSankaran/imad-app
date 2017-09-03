7a4643c78888a03180a5348e093c0b2779fb9e06
vijayakumarSankaran <30766520+vijayakumarSankaran@users.noreply.github.com>
2017-09-03 13:01:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.get('/', function (req, res) {
 
 
 app.get('/article-one', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send(createTemplate(article-one));
 });
 
 app.get('/article-two', function (req, res) {
