426f1cdf6f0a6c66c87e5f6e7051ce99a2023171
vijayakumarSankaran <30766520+vijayakumarSankaran@users.noreply.github.com>
2017-09-19 08:50:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.get('/student-db', function(req, res) {
             res.status(500).send(err.toString());
         }
         else {
-            res.send(JSON.stringify(result));
+            res.send(JSON.stringify(result.rows));
         }
     });
 });
